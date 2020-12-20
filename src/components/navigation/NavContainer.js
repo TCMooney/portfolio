b991942dfe42f14f07c434e5177aa04e562f47e7
TCMooney <tcmooney88@gmail.com>
2020-12-20 14:33:57 -0500

    added styles to Navigation component

--- src/components/navigation/NavContainer.js
+++ src/components/navigation/NavContainer.js
@@ -4,23 +4,24 @@ import { Link } from 'react-router-dom';
 export default function NavContainer() {
   return (
     <div className='nav-container'>
-      <div className='nav-left-side'>
-        <div className='nav-link'>
-          <Link to='/'>
+      <div className='nav-wrapper'>
+        <div className='nav-left-side'>
+          <Link className='nav-link' to='/'>
             Home
         </Link>
-        </div>
-        <div className='nav-link'>
-          <Link to='/about'>
+          <Link className='nav-link' to='/about'>
             About
         </Link>
-        </div>
-        <div className='nav-link'>
-          <Link to='/contact'>
+          <Link className='nav-link' to='/contact'>
             Contact
         </Link>
         </div>
+        <div className='nav-right-side'>
+          Tom Mooney
       </div>
+      </div>
+      <div className='nav-separator' />
     </div>
+
   )
 }
